41e8ee3be38ca62ac67211f44439677282a37ef6
Shuhei Hiya <syuhei176@gmail.com>
2020-04-18 12:46:14 +0900

    haskell

---
+++ helper/math.hs
@@ -0,0 +1,16 @@
+-- helper functions of ethereum 2.0 spec
+
+-- Return the largest integer x such that x**2 <= n.
+integerSquareroot :: Float -> Float
+integerSquareroot n = integerSquarerootLoop n n (fromInteger (floor ((n + 1) / 2)))
+
+integerSquarerootLoop :: Float -> Float -> Float -> Float
+integerSquarerootLoop n x y = 
+  if y >= x
+    then do
+      x
+    else do
+      integerSquarerootLoop n y (fromInteger (floor ((y + n / y) / 2)))
+
+main = do
+  print (integerSquareroot 10)
